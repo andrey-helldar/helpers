5dc152f94700b7c651c259d06977da7e332f6306
Andrey Helldar <helldar@ai-rus.com>
2017-09-19 01:15:42 +0300

    Add checks whether a file or directory exists on URL helper

---
+++ src/Support/Files.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Helldar\Helpers\Support;
+
+class Files
+{
+    /**
+     * Checks whether a file or directory exists on URL.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public static function urlFileExists($path)
+    {
+        $headers = @get_headers($path);
+
+        return (bool) stripos(reset($headers), '200 OK');
+    }
+}
--- src/helpers.php
+++ src/helpers.php
@@ -138,3 +138,17 @@ if (!function_exists('is_linux')) {
         return \Helldar\Helpers\Support\System::isLinux();
     }
 }
+
+if (!function_exists('url_file_exists')) {
+    /**
+     * Checks whether a file or directory exists on URL.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    function url_file_exists($path)
+    {
+        return \Helldar\Helpers\Support\Files::urlFileExists($path);
+    }
+}
