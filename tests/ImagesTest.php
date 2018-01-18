430f2a8248bd0463da88145870165419c891e97c
Andrey Helldar <helldar@ai-rus.com>
2018-01-18 20:52:52 +0300

    Add test rules for Images

--- tests/ImagesTest.php
+++ tests/ImagesTest.php
@@ -15,5 +15,8 @@ class ImagesTest extends AbstractTestCase
 
         $this->assertEquals($no_image, (new Images('unknown.txt'))->imageOrDefault($no_image));
         $this->assertEquals(null, (new Images('unknown.txt'))->imageOrDefault());
+
+        $this->assertEquals(null, (new Images('http://u.loc/unknown.txt'))->imageOrDefault());
+        $this->assertEquals($no_image, (new Images('http://u.loc/unknown.txt'))->imageOrDefault($no_image));
     }
 }
