d477976d6499dd8a841ab63a9e878ea453e0e313
Andrey Helldar <helldar@ai-rus.com>
2018-05-04 17:56:24 +0300

    Publication config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -16,7 +16,9 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__ . '/config/settings.php' => config_path('helpers.php'),
+        ]);
     }
 
     /**
@@ -24,7 +26,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom(__DIR__ . '/config/settings.php', 'helpers');
     }
 
     /**
