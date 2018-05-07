e8ef88785998a2149d53fb3cd0d13d0016ad5c1b
Andrey Helldar <helldar@ai-rus.com>
2018-05-07 11:59:22 +0300

    Remove unused variables

--- src/config/helpers.php
+++ src/config/helpers.php
@@ -5,9 +5,5 @@ return [
         'slack' => [
             'webhook' => env('SLACK_WEBHOOK_LOGS'),
         ],
-        'email' => [
-            'from' => env('MAIL_FROM_ADDRESS'),
-            'to' => env('MAIL_ADMIN_ADDRESS'),
-        ],
     ],
 ];
