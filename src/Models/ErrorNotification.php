d544eac579a7648cef0a0b631ce88938d4bc53ee
Andrey Helldar <helldar@ai-rus.com>
2018-07-11 17:49:03 +0300

    Added flattenable exceptions helper

--- src/Models/ErrorNotification.php
+++ src/Models/ErrorNotification.php
@@ -2,14 +2,24 @@
 
 namespace Helldar\Helpers\Models;
 
+use Helldar\Helpers\Traits\FlattenException;
 use Illuminate\Database\Eloquent\Model;
 
 class ErrorNotification extends Model
 {
+    use FlattenException;
+
     protected $fillable = ['parent', 'exception'];
 
+    /**
+     * @param \Exception $value
+     *
+     * @throws \ReflectionException
+     */
     protected function setExceptionAttribute($value)
     {
+        $this->flattenExceptionBacktrace($value);
+
         $this->attributes['exception'] = serialize($value);
     }
 
